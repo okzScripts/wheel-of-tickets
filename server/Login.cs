9d8d0bf6d92a55faff3f5052a4e23d5a08a64602
okzScripts <oskar.krantz90@gmail.com>
2025-02-06 14:04:17 +0100

    Testar APi call

--- server/Login.cs
+++ server/Login.cs
@@ -5,20 +5,24 @@
 public class Login
 {
     private List<Persons> persons = new List<Persons>{new Persons("kalle@anka.com", "1234", Roles.admin),
-                                                 {new Persons("peter@nicklas.com", "1234", Roles.support)},
-                                                 {new Persons("donald@anka.com", "1234", Roles.customer)}};
-public Login(){
+                                                 new Persons("peter@nicklas.com", "1234", Roles.support),
+                                                 new Persons("donald@anka.com", "1234", Roles.customer)};
+    public Login()
+    {
 
-}
-
-public string CheckLogin(string inputEmail, string inputPassword){
-foreach(var peon in persons){
-        if(inputEmail==peon.email&& inputPassword==peon.passWord){
-            return peon.role.ToString(); 
-        }
     }
-    return "no match"; 
-}
+
+    public string CheckLogin(string inputEmail, string inputPassword)
+    {
+        foreach (var peon in persons)
+        {
+            if (inputEmail == peon.email && inputPassword == peon.passWord)
+            {
+                return peon.role.ToString();
+            }
+        }
+        return "no match";
+    }
 
 }
 
--- server/Program.cs
+++ server/Program.cs
@@ -1,14 +1,21 @@
-using server; 
-
+using server;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var login= new Login(); 
 
-var role= login.CheckLogin("kalle@anka.com", "1234"); 
+
+Login login = new Login();
+
+var role = login.CheckLogin("kalle@anka.com", "1234");
 
 System.Console.WriteLine(role);
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/login", (HttpRequest request) =>
+{
+    return login.CheckLogin(email, password);
+}
+);
+
+
 
 app.Run();
