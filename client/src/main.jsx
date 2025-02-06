6235b01e84ba0257eca180a815ffde1bf2bf9eeb
ViktorThorsen <viktorthorsen92@gmail.com>
2025-02-06 10:29:53 +0100

    Adding users with API

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,34 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode, useState, use, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router"
+
+const UsersContext = createContext({})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<Hej/>
+    <App/>
   </StrictMode>,
 )
-
-function Hej() {
-  console.log("Hej")
+function App() {
+  const [users, setUsers] = useState([]);
+  return <UsersContext.Provider value={{ users, setUsers }}>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  </UsersContext.Provider>
+}
+function HomePage() {
+  const { users, setUsers } = use(UsersContext);
+  useEffect(() => {
+    fetch("/api/users")
+      .then(response => response.json()
+        .then(data => (setUsers(data))),[])
+  })
+  return <ul>
+    {users.map(user => (
+      <li>{ user.name}</li>
+        ))}
+  </ul>
 }
--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,20 @@
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<User> users = new()
+{
+    new(1, "Viktor", "indian@gmail.com", "hejhej", "Tombula", "Admin", true),
+};
+
+app.MapGet("/api/users", GetUsers);
 
 app.Run();
+
+
+async Task<List<User>> GetUsers()
+{
+    return users;
+}
+
+record User(int id, string name, string email, string password, string company, string role, bool active);
