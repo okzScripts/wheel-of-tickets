3618aae4b0c46fff90213eb77bf4066c5bc6dde1
srecko radivojevic <Momfy86@gmail.com>
2025-02-06 10:52:12 +0100

    initail commit

---
+++ server/app.cs
@@ -0,0 +1,20 @@
+﻿namespace server;
+
+
+
+public class app
+{
+    private List<users> Users = new List<users>{new users("kalle@anka.com", "1234", Roles.admin),
+                                                 {new users("peter@nicklas.com", "1234", Roles.admin)},
+                                                 {new users("donald@anka.com", "1234", Roles.admin)}};
+
+}
+
+record users(string email, string passWord, Roles role);
+
+enum Roles
+{
+    admin,
+    customer,
+    support
+}
