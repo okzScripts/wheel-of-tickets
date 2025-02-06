5bdb396094a26935816d8c7b7c21115cf240a249
huineith <westholm.jacob@gmail.com>
2025-02-06 13:21:52 +0100

    Prototype check loggin in backend w/o database

--- server/Login.cs
+++ server/Login.cs
@@ -2,15 +2,28 @@
 
 
 
-public class app
+public class Login
 {
-    private List<users> Users = new List<users>{new users("kalle@anka.com", "1234", Roles.admin),
-                                                 {new users("peter@nicklas.com", "1234", Roles.admin)},
-                                                 {new users("donald@anka.com", "1234", Roles.admin)}};
+    private List<Persons> persons = new List<Persons>{new Persons("kalle@anka.com", "1234", Roles.admin),
+                                                 {new Persons("peter@nicklas.com", "1234", Roles.support)},
+                                                 {new Persons("donald@anka.com", "1234", Roles.customer)}};
+public Login(){
 
 }
 
-record users(string email, string passWord, Roles role);
+public string CheckLogin(string inputEmail, string inputPassword){
+foreach(var peon in persons){
+        if(inputEmail==peon.email&& inputPassword==peon.passWord){
+            return peon.role.ToString(); 
+        }
+    }
+    return "no match"; 
+}
+
+}
+
+
+record Persons(string email, string passWord, Roles role);
 
 enum Roles
 {
--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,14 @@
+using server; 
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var login= new Login(); 
+
+var role= login.CheckLogin("kalle@anka.com", "1234"); 
+
+System.Console.WriteLine(role);
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
