8f7ed0d7459646bb547e6df2e5254025911a1106
okzScripts <oskar.krantz90@gmail.com>
2025-02-10 11:26:10 +0100

    ...

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,31 @@
+using server;
+using Npgsql;
+
+
 var builder = WebApplication.CreateBuilder(args);
+Database database = new();
+
+
+
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/users/{email}", GetUserById);
+
+
+
+
 
 app.Run();
+
+
+async Task<User> GetUserById(string mail)
+{
+
+
+}
+
+public record User(int id, string name, string email, string password, int company, int role, bool isactive);
+
