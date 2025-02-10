b8c3de3787e4e63457aeadce71dedf3ecbfed228
srecko radivojevic <Momfy86@gmail.com>
2025-02-10 13:20:08 +0100

    Added addAdmin method
    created a query for adding admin

---
+++ server/Admins.cs
@@ -0,0 +1,45 @@
+﻿using Npgsql;
+
+namespace server;
+
+public static class Admins
+{
+    
+    
+    
+    public static async Task<bool> addAdmin(string name, string email, string password, int company, NpgsqlDataSource db)
+    {
+
+        var role = Roles.admin;
+
+        await using var cmd = db.CreateCommand();
+            cmd.CommandText ="INSERT INTO users (name, email, password, company, role) VALUES ($1, $2 $3, $4, $5)";
+
+            cmd.Parameters.AddWithValue(name);
+            cmd.Parameters.AddWithValue(email);
+            cmd.Parameters.AddWithValue(password);
+            cmd.Parameters.AddWithValue(company);
+            cmd.Parameters.AddWithValue((int)role);
+
+            var result = await cmd.ExecuteScalarAsync();
+            if (result != null)
+            {
+                Console.WriteLine($"Admin created with ID: {result}");
+                
+                return true;
+            }
+
+            Console.WriteLine("Failed to insert admin into database.");
+            return false;
+
+    }
+    
+}
+
+enum Roles
+{
+    customer = 1,
+    support = 2,
+    admin = 3,
+    superAdmin = 4
+}
--- server/ServerActions.cs
+++ server/ServerActions.cs
@@ -12,6 +12,16 @@ public class ServerActions
     {
         db = database.Connection(); 
        
+        app.MapPost("api/users/admin", async (HttpContext context) =>
+        {
+            var requestBody = await context.Request.ReadFromJsonAsync<Move>();
+            if (requestBody?.tile is null || requestBody?.player is null || requestBody?.game is null || requestBody?.value is null)
+            {
+                return Results.BadRequest("tile (index), player (id), game (id), and value are required.");
+            }
+            bool success = await PlayTile(requestBody.tile, requestBody.player, requestBody.game, requestBody.value);
+            return success ? Results.Ok(true) : Results.Ok(false);
+        });
     }
     
 }
