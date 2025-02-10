c9fb49e17369cf311b691f7b19a34d9769ec1de1
okzScripts <oskar.krantz90@gmail.com>
2025-02-10 10:34:58 +0100

    login page creation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
+import { StrictMode, use, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+function LoginPage() {
+  return (
+
+    <main>
+      <h1>Login Page</h1>
+      <form>
+
+
+        <label htmlFor="email">E-post:</label>
+        <input type="email" id="email" />
+
+        <label htmlFor="password">Lösenord:</label>
+        <input type="password" id="password" />
+
+
+
+
+        <button type="submit">Registrera</button>
+      </form>
+    </main>
+
+  )
+}
+
+
