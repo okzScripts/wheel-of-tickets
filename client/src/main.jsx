58161b8cbb75f32f89d0c4ed56180a0287d1d334
ViktorThorsen <viktorthorsen92@gmail.com>
2025-02-11 10:47:00 +0100

    Adding superadminview.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,33 +5,9 @@ import {BrowserRouter , Routes,Route, useNavigate } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/> 
+    
   </StrictMode>,
 );
-
-
-function App(){
-  const [role,setRole]=useState(" "); 
-
-  return (<main> 
-         <span></span> <input id="email-field" placeholder="email"></input>
-          <input id="password-field"placeholder="password"></input>
-          <button onClick={Login } >Login</button>
-         </main>); 
-};
-
-async function  Login(){
-  let role=""; 
-  let email= document.getElementById("email-field").value; 
-  let password=document.getElementById("password-field").value;  
-  let route= `/api/login/?email=${email}&password=${password}`; 
-  console.log(route)
- await fetch(route)
-  .then(response=>response.text())
-        .then(data=>{role=data});
-
-  console.log(role);
-}
 
 
 
