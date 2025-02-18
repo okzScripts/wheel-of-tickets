c501b52ee02e58390ebbb939a6d22aa5409d98dc
ViktorThorsen <viktorthorsen92@gmail.com>
2025-02-18 12:36:01 +0100

    Fixing bug with earlier merge

--- server/Program.cs
+++ server/Program.cs
@@ -11,10 +11,15 @@ builder.Services.AddSingleton<NpgsqlDataSource>(db);
 
 var app = builder.Build();
 app.MapGet("/api/companies", CompanyRoutes.GetCompanies);
+app.MapGet("/api/companies/{email}", CompanyRoutes.GetCompany);
+app.MapPost("/api/companies", CompanyRoutes.AddCompany);
+app.MapPut("/api/companies/{previousEmail}", CompanyRoutes.EditCompany);
+app.MapPut("/api/companies/block/{email}/{active}", CompanyRoutes.BlockCompany);
 app.MapGet("/api/users/{role}", UserRoutes.GetUsers);
 app.MapGet("/api/users/{role}/{email}", UserRoutes.GetUser);
-app.MapPut("/api/users/{email}/{active}", UserRoutes.BlockUser);
+app.MapPut("/api/users/block/{email}/{active}", UserRoutes.BlockUser);
 app.MapPost("/api/users/{role}", UserRoutes.AddUser);
+app.MapPut("/api/users/{role}/{previousEmail}", UserRoutes.EditUser);
 //app.MapPost("/api/create-user", UserRoutes.AddUser);
 
 
