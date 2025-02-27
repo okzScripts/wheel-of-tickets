79e5fc0e810a7470a88c04e560c7729f8bb79f3f
jManniCode <jonas_manni@hotmail.com>
2025-02-27 13:41:30 +0100

    Database connection

--- server/Program.cs
+++ server/Program.cs
@@ -3,7 +3,7 @@ using server;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var dataSourceBuilder = new NpgsqlDataSourceBuilder("Host=localhost;Database=session_db;Username=postgres;Password=Playa717;Host=5432");
+var dataSourceBuilder = new NpgsqlDataSourceBuilder("Host=localhost;Database=hindu_wheels;Username=postgres;Password=1234;Port=5432");
 dataSourceBuilder.MapEnum<UserRole>();
 var db = dataSourceBuilder.Build();
 
