83ff95df7891ff4890d1e2cb2b1e1dad5e7120e2
huineith <westholm.jacob@gmail.com>
2025-03-13 10:50:22 +0100

    Added route to reset mockpasswords

--- server/MockHasher.cs
+++ server/MockHasher.cs
@@ -69,6 +69,52 @@ public class MockHasher
 
 
 
+    public static async Task<IResult> ResetMockPasswords(NpgsqlDataSource db)
+    {
+        List<string> originalPasswords =new(){
+        "password123", 
+        "password123", 
+        "password123", 
+        "password123",
+        "auwodh",
+        "aiwdj",  
+        "hejhej",
+        "password123", 
+        "nalle",
+         "styre", 
+        "nuff",  
+        "asd123", 
+        "aswe", 
+        "best", 
+        "kung",
+        "123"
+        };
+           using var conn = await db.OpenConnectionAsync();
+                using var transaction = await conn.BeginTransactionAsync();
+
+
+        for (int i = 0; i < originalPasswords.Count; i++)
+        {
+
+            using var cmd2 = db.CreateCommand("UPDATE users SET password = $1 WHERE id = $2");
+            cmd2.Parameters.AddWithValue(originalPasswords[i]);
+            cmd2.Parameters.AddWithValue(i + 1);
+
+            int? rowsAffected = await cmd2.ExecuteNonQueryAsync();
+            if (rowsAffected == 0)
+            {
+                await transaction.RollbackAsync();
+                Console.WriteLine("No rows affected");
+                return TypedResults.BadRequest("Njiet!");
+            }
+        }
+        await transaction.CommitAsync();
+        return Results.Ok(originalPasswords.Count);
+
+    }
+
+
+
 }
 
 
--- server/Program.cs
+++ server/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var dataSourceBuilder = new NpgsqlDataSourceBuilder("Host=localhost;Database=swine_sync;Username=postgres;Password=1234;Port=5432");
+var dataSourceBuilder = new NpgsqlDataSourceBuilder("Host=localhost;Database=swine_sync;Username=postgres;Password=portedinme;Port=5432");
 dataSourceBuilder.MapEnum<UserRole>();
 var db = dataSourceBuilder.Build();
 
@@ -64,6 +64,7 @@ app.MapPut("/api/tickets/rating/{id}", TicketRoutes.TicketRating);
 
 //ANVÄNDS BARA 1 GÅNG!!!
 app.MapPost("/api/password/mockhash/", MockHasher.HashMockPasswords);
+app.MapPost("api/password/mockreset/",MockHasher.ResetMockPasswords); 
 //ANVÄNDS BARA 1 GÅNG!!!
 
 app.MapGet("/api/messages/{id}", MessageRoutes.GetTicketMessages);
